dec6720475a2b675cc049f9bd8a2c1be40bfc866
Krystal Mitchell <10262399@my.uvu.edu>
2020-03-16 00:31:11 -0600

    auth controller

--- routes/middleware/is-auth.js
+++ routes/middleware/is-auth.js
@@ -3,20 +3,20 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   const authHeader = req.get("Authorization");
   if (!authHeader) {
-    const error = new Error("Not authenticated.");
+    const error = new Error("Not authenticated yet...dun dun dunnn");
     error.statusCode = 401;
     throw error;
   }
   const token = authHeader.split(" ")[1];
   let decodedToken;
   try {
-    decodedToken = jwt.verify(token, "somesupersecretsecret");
+    decodedToken = jwt.verify(token, "hamakabula");
   } catch (err) {
     err.statusCode = 500;
     throw err;
   }
   if (!decodedToken) {
-    const error = new Error("Not authenticated.");
+    const error = new Error("Not authenticated yet...dun dun dunnn");
     error.statusCode = 401;
     throw error;
   }
